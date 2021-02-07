ea77c55324c99711989386d8e153409fd068935d
ronchabarr <ronchabarr@gmail.com>
2021-02-07 16:23:54 +0200

    GameBase

---
+++ Assets/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    QuizManager _quizManager;
+
+    [SerializeField] float RoundTime;
+
+    public void Start()
+    {
+        InitQuiz();
+
+    }
+    
+
+    public void InitQuiz()
+    {
+        _quizManager = QuizManager.Instance;
+        _quizManager._gameManager = this;
+    }
+
+    public void ReloadRound()
+    {
+
+    }
+        
+    public IEnumerator RoundTimer()
+    {
+        yield return new WaitForSeconds(RoundTime);
+        RoundOver();
+
+    }
+    public void SendNewQuestion()
+    {
+
+    }
+    public void RoundOver()
+    {
+
+    } 
+    public void SetRegistered()
+    {
+
+    }
+    
+
+
+    
+
+
+
+}
---
+++ Assets/QuizManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ public class QuizManager : MonoBehaviour
+{
+
+        internal GameManager _gameManager;
+
+
+
+
+        private static QuizManager _instance;
+
+        public static QuizManager Instance { get { return _instance; } }
+
+
+        private void Awake()
+        {
+            if (_instance != null && _instance != this)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                _instance = this;
+            }
+        }
+    }
+
+
+
+
+
